b86c11bbe63cba8208ff07f9dd1987e522eebbb1
hotuewa <winiar.2003@op.pl>
2024-10-25 22:34:22 +0200

    zadanie 10

---
+++ Lab1/zad10.py
@@ -0,0 +1,22 @@
+x=int(input("Podaj 1 liczbe:"))
+y=int(input("Podaj 2 liczbe:"))
+z=int(input("Podaj 3 liczbe:"))
+
+if x<y and x<z:
+    print(x)
+    if y<z:
+        print(y,z)
+    else:
+        print(z,"\n",y)
+elif y<x and y<z:
+    print(y)
+    if x < z:
+        print(x, z)
+    else:
+        print(z, "\n", x)
+elif z<x and z<y:
+    print(z)
+    if x < y:
+        print(x, y)
+    else:
+        print(y, "\n", x)
