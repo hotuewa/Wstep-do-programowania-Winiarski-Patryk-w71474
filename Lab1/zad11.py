574cf5e1a75cc27d8b6bbdf5f6c53fde8dbdc5fd
hotuewa <winiar.2003@op.pl>
2024-11-14 11:49:48 +0100

    update zad 11

--- Lab1/zad11.py
+++ Lab1/zad11.py
@@ -5,15 +5,16 @@ a = int(input("podaj a:"))
 b = int(input("Podaj b: "))
 c = int(input("Podaj c: "))
 delta = (b ** 2 - 4 * a * c)
+print(delta)
 if int(delta) > 0:
     print("Pierwiastki równania kwadratowego: ")
-    x1 = (- b - math.sqrt(int(delta))) / 2 * a
-    x2 = (- b + math.sqrt(int(delta))) / 2 * a
+    x1 = (- b - math.sqrt(int(delta))) / (2 * a)
+    x2 = (- b + math.sqrt(int(delta))) / (2 * a)
     print("x1: ",  x1)
     print("x2: ", x2)
 elif int(delta) == 0:
     print("Pierwiastki równania kwadratowego: ")
-    x0 = - b / 2 * a
+    x0 = - b / (2 * a)
     print("x0: ",  x0)
 else:
     print("Delta jest ujemna, wiec nie liczymy miejsc zerowych.")
